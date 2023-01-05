fc350b4ee2943add17cddeb97e26852cdf459cf8
Ray-linux <ranirava52@gmail.com>
2023-01-05 21:00:48 +0530

    adding task cards

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './tasks.css'
+import {FcCheckmark} from 'react-icons/fc'
+import {HiXMark} from 'react-icons/hi2'
 
 export default function Tasks(props) {
 
@@ -7,13 +9,15 @@ export default function Tasks(props) {
     <>
     <div className="tasks">
       <div className="task_title">
-        <h3>Hello World</h3>
+        <h3>{props.tasks.title}</h3>
         </div>
-      <div className="task_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis asperiores a autem.I</div>
-      <div className="task_btn">
-        tme: 2.30pm
-        <button>Submit</button>
-        <button>restart</button>
+      <div className="task_desc">{props.tasks.desc}</div>
+      <div className="task_footer">
+        <h4>tme: 2.30pm </h4>
+        <div className="task_btns">
+        <button className='task_btn'><FcCheckmark/></button>
+        <button className='task_btn'><HiXMark/></button>
+        </div>
       </div>
     </div>
     </>
